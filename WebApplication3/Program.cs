5dfdb67fb008356769a5001eb3555611a4a3851a
Hildan Mulyo <88073895+Hildan2002@users.noreply.github.com>
2023-09-30 13:26:56 +0700

    bersih2 comment

--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -7,16 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("AzureCS")));
-/*options.UseSqlServer(
-    builder.Configuration.GetConnectionString("Default"), 
-    sqlServerOptionsAction: sqlOptions =>
-    {
-        sqlOptions.EnableRetryOnFailure(
-                maxRetryCount: 5,
-                maxRetryDelay: TimeSpan.FromSeconds(30),
-                errorNumbersToAdd: null);
-    })
-);*/
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
